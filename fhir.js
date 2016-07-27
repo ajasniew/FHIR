2e7746d927bb57c5a50f8c33e60731452a5a9360
alina <alina.jasniewski@stonybrook.edu>
2016-07-27 11:28:34 -0400

    small modifications

--- fhir.js
+++ fhir.js
@@ -1,93 +1,84 @@
-//Jonas code
-console.log('on fihr.js')
+//base on Jonas code
+console.log('on fihr.js');
 FHIR = function(url) {
     if (!url) {
-        url = "https://open-api.fhir.me/"
+        url = "https://open-api.fhir.me/";
     }
-    this.url = url
+    this.url = url;
 
     this.parmUnpack = function(x) {
-        y = ''
+        y = '';
         if (Array.isArray(x)) {
             x.forEach(function(xi) {
-                y += xi + '&'
+                y += xi + '&';
             })
         } else {
-            // assume Object
             for (var p in x) {
-                y += p + '=' + x[p] + '&'
+                y += p + '=' + x[p] + '&';
             }
         }
-        return y
+        return y;
     }
 
-    this.getJSON = function(url, h) {
-        // breaking free from jQuery          
+    this.getJSON = function(url, h) {        
         var getJSON = function(u, fun, onErr) {
             if (!u) {
-                u = this.url
+                u = this.url;
             }
             if (!fun) {
                 fun = function(x) {
-                    console.log('retrieved:', x)
+                    console.log('retrieved:', x);
                 }
             }
             if (!onErr) {
                 onErr = function(x) {
-                    console.log('error: ', x)
+                    console.log('error: ', x);
                 }
             }
-            var c = 0
-            var xhr = new XMLHttpRequest()
-            xhr.open('get', u, true)
-            // protocol, target url and assynchronicity
-            xhr.send()
-            /*
-                xhr.onprogress=function(){
-                    c++
-                    console.log(c+') in progress at ',Date())
-                }
-                */
+            var c = 0;
+            var xhr = new XMLHttpRequest();
+            xhr.open('get', u, true);
+            // protocol, url, assynchronic call
+            xhr.send();
             xhr.onload = function(x) {
                 try {
-                    fun(JSON.parse(x.target.response))
+                    fun(JSON.parse(x.target.response));
                 } catch (e) {
-                    onErr(e)
+                    onErr(e);
                 }
             }
-            xhr.onerror = function(x) {// I don't understand who's listening to this event
+            xhr.onerror = function(x) {
             }
         }
         return new Promise(function(resolve, reject) {
             getJSON(url, function(x) {
-                resolve(x)
+                resolve(x);
             }, function(x) {
-                reject(x)
+                reject(x);
             })
         }
         )
     }
 
     this.Patient = function(uid, fun) {
-        //if(!fun){fun = function(x){console.log(x)}}
         if (fun) {
             return this.Patient(uid).then(function(x) {
-                fun(x)
+                fun(x);
             }).catch(function(e) {
-                console.log('error: ', e)
+                console.log('error: ', e);
             })
         } else {
             if (!uid) {
                 // no uid provided, get the list
-                return this.getJSON(this.url + "Patient?_format=json")
+                return this.getJSON(this.url + "Patient?_format=json");
             } else {
                 if (typeof (uid) == "string") {
-                    return this.getJSON(this.url + 'Patient/' + uid + '?_format=json')
+                    return this.getJSON(this.url + 'Patient/' + uid + '?_format=json');
                 } else if (uid.length == 0) {
-                    // to allow for empty Arrays a la matlab
-                    return this.Patient(false)
+                    // to allow for empty Arrays
+                    return this.Patient(false);
                 } else {
-                    return this.getJSON(this.url + 'Patient/?' + this.parmUnpack(uid) + '_format=json')
+                    return this.getJSON(this.url + 'Patient/?' + this.parmUnpack(uid) + '_format=json');
                 }
             }
         }
@@ -95,42 +86,40 @@ FHIR = function(url) {
 
     this.pre=function(x,id){ // show JSON in a <pre> element, create one if t doesn't exist
         if(!id){
-            id='FHIRpre'
+            id='FHIRpre';
         }
         if(!document.getElementById(id)){
-            var pre = document.createElement('pre')
-            pre.id=id
-            document.body.appendChild(pre)
+            var pre = document.createElement('pre');
+            pre.id=id;
+            document.body.appendChild(pre);
         }
-        var pre = document.getElementById(id)
-        pre.innerHTML=JSON.stringify(x,null,3)
-        pre.style.color='blue'
-        4
+        var pre = document.getElementById(id);
+        pre.innerHTML=JSON.stringify(x,null,3);
+        pre.style.color='blue';
+        4;
 
     }
-    // ...
 }
 
-
-/// UI tests
+// UI tests
 
 ui1 = function(){
     // list patients
-    var x = new FHIR()
-    var div = document.createElement('div')
-    div.id='ui1'
-    document.body.appendChild(div)
-    var h = '<button id="getPatientData">Get Patient data</button><pre id="FHIRpre"></pre>'
-    div.innerHTML=h
-    var getPatientDataButton = document.getElementById('getPatientData')
+    var x = new FHIR();
+    var div = document.createElement('div');
+    div.id='ui1';
+    document.body.appendChild(div);
+    var h = '<button id="getPatientData">Get Patient data</button><pre id="FHIRpre"></pre>';
+    div.innerHTML=h;
+    var getPatientDataButton = document.getElementById('getPatientData');
     getPatientDataButton.onclick=function(){
         x.Patient()
          .then(function(dt){
-             x.pre(dt)
+             x.pre(dt);
          })
     }
     
-    4
+    4;
 }
 
 
